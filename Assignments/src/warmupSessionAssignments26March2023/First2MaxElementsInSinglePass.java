3e57f0878b21e5b490039f71c3e5f7636ab47de7
surajkaushik <kaushiksuraj1102@gmail.com>
2023-04-01 14:17:01 +0530

    1st April Class Questions done

---
+++ Assignments/src/warmupSessionAssignments26March2023/First2MaxElementsInSinglePass.java
@@ -0,0 +1,31 @@
+package warmupSessionAssignments26March2023;
+
+public class First2MaxElementsInSinglePass {
+
+	public static void main(String[] args) {
+		int input[]= {21,32,-1,23,0,423,400,21,8,-2,900};
+		First2MaxElementsInSinglePass.find2Max(input);
+	}
+
+	private static void find2Max(int[] input) {
+		if(input==null|| input.length==0 ||input.length==1) {
+			System.out.println("Size is 0 || Null || size is 1");
+			return;
+		}
+		int max1=Integer.MIN_VALUE;
+		int max2=Integer.MIN_VALUE;
+		for(int i=0;i<input.length;i++) {
+			if(input[i]>max1) {
+				max2=max1;
+				max1=input[i];
+			}
+			else if(input[i]>max2 && max2!=max1) {
+				max2=input[i];
+			}
+		}
+		System.out.println("Max 1 :"+max1);
+		System.out.println("Max 2 :"+max2);
+		
+	}
+
+}
--- Assignments/src/warmupSessionAssignments26March2023/MaxElementOfArray.java
+++ Assignments/src/warmupSessionAssignments26March2023/MaxElementOfArray.java
@@ -5,10 +5,36 @@ import java.util.Arrays;
 public class MaxElementOfArray {
 
 	public static void main(String[] args) {
-		int a[]= {2,54,23,50,1,5,23};
-		Arrays.sort(a);
-		System.out.println(a[a.length-1]);
-
+		//int a[] = { 2, -54, -23, 50, 1, 5, 23 };
+		//int a[]= new int[0]; // ----- Size is Zero 0
+		//int a[]=null; // ---- Null 
+		int a[]= {}; // Size is 0
+		if(a==null || a.length==0) {
+			if(a==null) {System.out.println("Null");}
+			else if(a.length==0) {System.out.println("Size 0");}
+			return;
+		}
+		
+		int min=a[0],max=a[0];
+		for(int i=1;i<a.length;i++) {
+			if(a[i]>max) {max=a[i];}
+			if(a[i]<min) {min=a[i];}
+		}
+		System.out.println("Min:"+min);
+		System.out.println("Max:"+max);
+	
+		
+		
+//		Arrays.sort(a);
+//		System.out.println(a[a.length-1]);
+		
+//		int max = a[0];
+//		for (int i : a) {
+//			if (max < i) {
+//				max = i;
+//			}
+//		}
+//		System.out.println(max);
 	}
 
 }
